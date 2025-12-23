d96c7b5fa3a2cf5adb23f4e3200608a172f603e3
yahoo-afk <sjffhdfg@gmail.com>
2025-12-23 09:45:30 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- C++/프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cpp
+++ C++/프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cpp
@@ -1,17 +1,9 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
 int solution(int num1, int num2) {
-    int sum = 0;
-    sum = num1 + num2;
- cout<< sum<<endl;
-    return sum;
-}
-int main(){
-    int n1;
-    int n2;
-    cin>> n1>>n2;
-    solution(n1,n2);
+    int answer = -1;
+    return answer;
 }
