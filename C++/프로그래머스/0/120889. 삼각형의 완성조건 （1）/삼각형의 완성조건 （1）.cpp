8642b53fd0ddd80f87913dd5c5620581ca1592be
yahoo-afk <sjffhdfg@gmail.com>
2026-01-01 13:03:18 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> sides) {
+    int answer = 0;
+    sort(sides.begin(),sides.end());
+        if((sides[0]+sides[1])<=sides[2])
+        {
+            return 2;
+        }else
+        {return 1;}
+}
