50e601a5e7931303e307b5cb2c5cb8f9fea408fc
yahoo-afk <sjffhdfg@gmail.com>
2026-01-08 09:25:49 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+   string answer = "";
+    int a = s.size() / 2;
+    if (s.size() % 2 == 0)
+    {
+        answer +=s[a - 1];
+        answer += s[a]; 
+        
+    }
+    else if (s.size() % 2 != 0)
+    {
+        answer = s[a];
+    }
+    return answer;
+}
