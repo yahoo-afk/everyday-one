eaf96ce1e8e1f274bd5eec30c68780795230046f
yahoo-afk <sjffhdfg@gmail.com>
2026-06-30 19:41:41 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/1/12928. 약수의 합/약수의 합.cpp
+++ C++/프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -1,21 +1,21 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
 int solution(int n) {
-    int sum = 0;
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            sum += n / i;
+    int answer = 0;
+    if(n == 0)
+    {
+        return 0;
+    }
+    for(int i=1; i<=n; ++i)
+    {
+        if(n%i == 0)
+        {
+            answer +=i;
         }
     }
-    return sum;
-}
-int main()
-{
-    int n;
-    cin >> n;
-    cout << solution(n) << endl;
-
+    
+    return answer;
 }
