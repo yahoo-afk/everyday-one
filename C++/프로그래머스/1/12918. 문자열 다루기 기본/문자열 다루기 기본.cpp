bb7b7add3336a4efeb13dab06e30b120359a4349
yahoo-afk <sjffhdfg@gmail.com>
2026-07-08 17:05:21 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

--- C++/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
+++ C++/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -3,24 +3,21 @@
 #include <cctype>
 using namespace std;
 
-bool solution(string s)
-{
-	bool answer = true;
-
-	if (s.size() != 4 && s.size() != 6)
-	{
-		answer = false;
-        return answer;
-	}
-
-	for (int i = 0; i < s.size(); i++)
-	{
-		if (!isdigit(s[i]))
-		{
-			answer = false;
-            return answer;
-		}
-	}
-    
-	return true;
+bool solution(string s) {
+    bool answer = true;
+    if(s.length() == 4 || s.length()==6)
+    {
+         for(int i=0; i<s.length(); ++i)
+         {
+         if(isalpha(s[i]))
+            {
+            return false;
+            }
+         }    
+    }
+    else
+    {
+        return false;
+    }
+    return answer;
 }
