936929e1ce17e85394c6ab223ca147583cc775b3
yahoo-afk <sjffhdfg@gmail.com>
2026-07-10 21:15:52 +0900

    [level 1] Title: 시저 암호, Time: 0.26 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/1/12926. 시저 암호/시저 암호.cpp
+++ C++/프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -4,25 +4,31 @@
 using namespace std;
 
 string solution(string s, int n) {
-	string answer = "";
-
-	//1문자열을다 문자로바꾼다
-	//2그리고나서 수를 더한다
-	//if()문으로 z가 넘어가면 어떻게 할지?
-	for (auto& it : s)
-	{
-		if (it >= 'A' && it <= 'Z')
-		{
-		    it = (it- 'A' + n) % 26+'A';
-            
-		}
-		else if (it >= 'a' && it <= 'z')
-		{
-			it = (it-'a' + n) % 26 +'a';
-            
-		}
-        answer = s;
-	}
-
-	return answer;
+    string answer = "";
+    for(int i=0; i<s.length(); ++i)
+    {
+        if(s[i] == ' ')
+        {
+            answer += ' ';
+        }
+        else if(s[i]>='A'&& s[i]<='Z')
+        {
+            int a = s[i]+n;
+            if(a>'Z')
+            {
+                a -=26;
+            }
+            answer += a;
+        }
+          else if(s[i]>='a'&& s[i]<='z')
+        {
+            int a = s[i]+n;
+            if(a>'z')
+            {
+                a -=26;
+            }
+              answer += a;
+        }
+    }
+    return answer;
 }
