d5994e7586c0bcb5fc9b406c0c18acfcaac1dc16
yahoo-afk <sjffhdfg@gmail.com>
2026-07-13 20:35:40 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.03 ms, Memory: 4.76 MB -BaekjoonHub

--- C++/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ C++/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -1,20 +1,18 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
+vector<string> solution(vector<string> strings, int n) {
+    sort(strings.begin(), strings.end(), [&](string a, string b) {
+        if (a[n] != b[n]) {
+            return a[n] < b[n];
+        } else {
+            return a < b;
+        }
+    });
+    
 
-
-vector<string> solution(vector<string> strings, int n)
-{
-	vector<string> answer;
-	//문자열 크기 비교 하는데 그냥 솔트못쓰나
-	{
-		sort(strings.begin(), strings.end(), [n](string a, string b) { return a[n] == b[n] ? (a < b) : (a[n] < b[n]); });
-	}
-	answer = strings;								//여기 3번째에 비교를 어떻게 할지를 설정해라?
-	//strings의 n번째 인덱스가 작은것부터 오게 해야하니
-	return answer;
+    return strings; 
 }
-    
-    
