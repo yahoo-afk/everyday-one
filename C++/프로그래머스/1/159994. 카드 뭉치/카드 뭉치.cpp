c45e24ba59fa292f46f014ce1372d771c51b34c8
yahoo-afk <sjffhdfg@gmail.com>
2026-07-14 14:40:55 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- C++/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ C++/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -4,24 +4,23 @@
 using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
-	string answer = "";
-	int idx1 = 0;
-	int idx2 = 0;
-	for (int i = 0; i < goal.size(); ++i)
-	{
-		if (idx1 < cards1.size() && goal[i] == cards1[idx1])
-		{
-			idx1++;
-		}
-		else if (idx2 < cards2.size() && goal[i] == cards2[idx2])
-		{
-			idx2++;
-		}
-		else
-		{
-			return answer = "No";
-		}
-	}
+    int idx1 = 0;
+    int idx2 = 0;
 
-	return "Yes";
+    for (const string& word : goal) {
+    
+        if (idx1 < cards1.size() && word == cards1[idx1]) {
+            idx1++;
+        }
+
+        else if (idx2 < cards2.size() && word == cards2[idx2]) {
+            idx2++;
+        }
+
+        else {
+            return "No";
+        }
+    }
+
+    return "Yes";
 }
