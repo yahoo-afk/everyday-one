7ef5f33e84b90d14493df92d588cee3b7ee27c6a
yahoo-afk <sjffhdfg@gmail.com>
2026-07-14 14:26:13 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.57 ms, Memory: 4.64 MB -BaekjoonHub

--- C++/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ C++/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -4,20 +4,19 @@
 using namespace std;
 
 vector<int> solution(int k, vector<int> score) {
-	vector<int> answer;
-	vector<int>temp;
-	for (int i = 0; i < score.size(); ++i)
-	{
-		temp.push_back(score[i]);
-		sort(temp.begin(), temp.end());
-
-		if (temp.size() > k)
-		{
-		    temp.erase(temp.begin());
-		}
-
-		answer.push_back(temp[0]);
-	}
-
-	return answer;
+    vector<int> answer;
+    vector<int> temp;
+    for(int i=0; i<score.size(); ++i)
+    {
+      temp.push_back(score[i]); 
+      sort(temp.begin(),temp.end(),greater<int>());
+      
+        if(temp.size()>k)
+          {
+              temp.pop_back();
+          }
+        answer.push_back(temp.back());
+    }
+    
+    return answer;
 }
